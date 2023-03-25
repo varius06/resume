245e7176b9b806a1d7d9a9124e9a588dc91d9d22
varius06 <varius06@mail.ru>
2023-03-25 12:44:13 +0300

    Программа резюме

---
+++ src/model/Resume.java
@@ -0,0 +1,33 @@
+package model;
+
+import java.util.Objects;
+
+public class Resume {
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Resume resume = (Resume) o;
+        return Objects.equals(uuid, resume.uuid);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(uuid);
+    }
+
+    private String uuid;
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    @Override
+    public String toString() {
+        return uuid;
+    }
+}
