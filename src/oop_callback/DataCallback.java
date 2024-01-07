23f872d0a85bfda95666f0fe73db1e2fa2fa8083
Varius06 <varius06@mail.ru>
2024-01-07 23:36:27 +0300

    opp basic add class callback

--- src/oop_callback/DataCallback.java
+++ src/oop_callback/DataCallback.java
@@ -6,4 +6,23 @@ public interface DataCallback {
 
     void returnError(ErrorInfo errorInfo);
 
+    class Base implements DataCallback{
+
+        private final Log log;
+
+        public Base(Log log) {
+            this.log = log;
+        }
+
+        @Override
+        public void returnSuccess(DataContainer dataContainer) {
+            dataContainer.show();
+        }
+
+        @Override
+        public void returnError(ErrorInfo errorInfo) {
+            errorInfo.log(log);
+        }
+    }
+
 }
--- src/oop_callback/TestCallback.java
+++ src/oop_callback/TestCallback.java
@@ -4,7 +4,8 @@ public class TestCallback {
     public static void main(String[] args) {
         final Log log = new Log.Local();
         Executor executor = new Executor();
-        DataCallback dataCallback = new DataCallback() {
+        DataCallback dataCallback = new DataCallback.Base(log);
+       /* DataCallback dataCallback = new DataCallback() {
             @Override
             public void returnSuccess(DataContainer dataContainer) {
                 dataContainer.show();
@@ -14,7 +15,7 @@ public class TestCallback {
             public void returnError(ErrorInfo errorInfo) {
                 errorInfo.log(log);
             }
-        };
+        };*/
         executor.execute(dataCallback);
     }
 }
